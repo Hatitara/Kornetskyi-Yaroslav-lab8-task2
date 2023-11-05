015b4d655c3c12880111d229f6e46a39fe536e3a
Hatitara <kornetskyi.pn@ucu.edu.ua>
2023-11-05 13:15:17 +0200

    Added check_row function

--- puzzle.py
+++ puzzle.py
@@ -30,6 +30,22 @@ def board_to_matrix(board: list[str]) -> list[list[str]]:
     return matrix
 
 
+def check_row(row: list[str]) -> bool:
+    '''
+    list[str] -> bool
+    Checks whether row is suitable for board.
+    >>> check_row(['*', '*', ' ', ' ', '3', '*', '*', '*', '*'])
+    True
+    >>> check_row(['*', '*', '*', '*', ' ', '*', '*', '*', '*'])
+    True
+    >>> check_row([' ', '6', ' ', '6', '8', '3', ' ', ' ', '*'])
+    False
+    '''
+    for_check = [el for el in row if el not in ('*', ' ')]
+    without_reps = list(set(for_check))
+    return len(for_check) == len(without_reps)
+
+
 def validate_board(board: list[str]) -> bool:
     pass
 
